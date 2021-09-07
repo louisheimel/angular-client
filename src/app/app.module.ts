3071c0a22cf644e86ddb01ef8d05d214e086903b
Louis Heimel <louis.heimel@calabrio.com>
2021-09-07 21:48:42 +0000

    add components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
+import { UploadComponent } from './upload/upload.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent
+    SignupComponent,
+    UploadComponent
   ],
   imports: [
     BrowserModule,
