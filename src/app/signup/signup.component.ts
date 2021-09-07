47d1ea94f99bda5d4206ee26f248587acdbaa856
Louis Heimel <louis.heimel@calabrio.com>
2021-09-07 22:43:58 +0000

    wire up form

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -8,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class SignupComponent implements OnInit {
 
   constructor() { }
+  
+  username: String = ""
+  password: String = ""
 
   ngOnInit(): void {
   }
